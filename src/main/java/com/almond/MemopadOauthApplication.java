c48f58026b9b0044a9c550874b7d59ddef188d2c
dlcndaks12 <dlcndaks12@naver.com>
2017-08-30 04:35:43 +0900

    init

---
+++ src/main/java/com/almond/MemopadOauthApplication.java
@@ -0,0 +1,12 @@
+package com.almond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemopadOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemopadOauthApplication.class, args);
+	}
+}
